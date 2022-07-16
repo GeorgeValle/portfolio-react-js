72eeb63c9aa785e8f623c90cfe4038cc40c77a50
GeorgeValle <jorgeguillermovalle@gmail.com>
2022-07-16 04:30:53 -0300

    slider incomplete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react';
 import Cover from "./components/cover/Cover";
 import Navbar from "./components/navbar/Navbar";
 import About from "./components/about/About";
+import Slider from "./components/slider/Slider";
 
 function App() {
 const [scrollHeight, setScrollHeight]= useState(0);
@@ -22,6 +23,7 @@ useEffect(() => {
       <Navbar isScrolling={scrollHeight} />
       <Cover/>
       <About/>
+      <Slider/>
     </div>
   );
 }
