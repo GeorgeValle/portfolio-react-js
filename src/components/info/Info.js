2cbe295d8fa805a17bc73c41b3e95b0b138ac4eb
GeorgeValle <jorgeguillermovalle@gmail.com>
2022-07-17 02:46:58 -0300

    info and Footer started

---
+++ src/components/info/Info.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Info.css";
+
+const Info = () => {
+    return (
+        <div className="info-container">
+            <div className="info">
+                <h1>Once you choose hope, anything&#8217;s possible</h1>
+                <h3>Traducci&#243;n: Cuando eliges la esperanza, todo es posible</h3>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Info
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = ({isScrolling}) => {
     return (
     <nav className={`navbar ${isScrolling> 20 ? "scrolling" : null}`}>
         <div className="navbar-logo" onClick={toTheTop}>
-            Your Name
+            Jorge Valle
         </div>
         
     </nav>
