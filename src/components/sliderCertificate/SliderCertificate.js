dfcda707ba6613a485e48f5a3cf219dda1eaead0
GeorgeValle <jorgeguillermovalle@gmail.com>
2022-07-22 00:25:17 -0300

    started carouusel

---
+++ src/components/sliderCertificate/SliderCertificate.js
@@ -0,0 +1,19 @@
+import {useState, useEffect} from "react";
+
+
+import React from 'react'
+
+const SliderCertificate = () => {
+
+    const images =["imagen1","Imagen2","imagen3"];
+    const [selectedIndex, SetSelectedIndex]=useState(0);
+    const [selectedImage, setSelected] =useState(image1);
+
+
+
+    return (
+    <div></div>
+    )
+}
+
+export default SliderCertificate
